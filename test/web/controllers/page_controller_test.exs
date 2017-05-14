04768e1ac05a9c6467aefe3f955372508d4b960f
Roman Chvanikov <chvanikoff@gmail.com>
2017-05-14 23:32:58 +0900

    Added failing part

--- test/web/controllers/page_controller_test.exs
+++ test/web/controllers/page_controller_test.exs
@@ -4,5 +4,7 @@ defmodule Demo.Web.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Response: {:ok"
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Response: {:ok"
   end
 end
